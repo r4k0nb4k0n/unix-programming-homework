6b250ee6c009eeeecfd98d390a3021d3ee9a38f7
Choe Hyeong Jin <chj6097@gmail.com>
2017-11-01 21:03:06 +0900

    Add setbuf, setvbuf functions

---
+++ 06.Std.IO/setbuf_setvbuf/README.md
@@ -0,0 +1,2 @@
+# setbuf & setvbuf functions
+
---
+++ 06.Std.IO/setbuf_setvbuf/setbuf.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+	char buf[BUFSIZ];
+
+	setvbuf(stdout, buf, _IOLBF, BUFSIZ);
+	// stdout도 버퍼를 새로 설정하면 Fully Buffered로 바뀜 
+	// 따라서 setvbuf(stdout, buf, _IOLBF, BUFSIZ);로 바뀜
+	printf("Hello,"); sleep(1);
+	printf("Unix!"); sleep(1);
+	printf("\n"); sleep(5);
+
+	setbuf(stdout, NULL);
+	sleep(1);
+	printf("How "); sleep(1);
+	printf("are "); sleep(1);
+	printf("you?"); sleep(1);
+	printf("\n"); sleep(1);
+
+	return 0;
+}
