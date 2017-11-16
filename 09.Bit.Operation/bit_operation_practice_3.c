e7d51142a84230eb1ad0a14cb08a10f06eae4563
Choe Hyeong Jin <chj6097@gmail.com>
2017-11-16 12:16:45 +0900

    Update bit_operation_practice_3.c

--- 09.Bit.Operation/bit_operation_practice_3.c
+++ 09.Bit.Operation/bit_operation_practice_3.c
@@ -3,8 +3,8 @@
 void div_num(unsigned int num, int *first, int *second, int *third){
 	unsigned int temp = num;
 	*first = num >> 22;
-	*second = (num << 10) >> 22;
-	*third = (num << 20) >> 20;
+	*second = (num << 10) >> 22; // (num >> 12) & 0x3ff;
+	*third = (num << 20) >> 20; // num & 0xfff;
 }
 
 int main(void){
