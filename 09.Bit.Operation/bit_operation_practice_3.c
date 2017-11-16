312802840b9dd99d85f20b87b47de0d5f0c20286
Choe Hyeong Jin <chj6097@gmail.com>
2017-11-16 12:14:35 +0900

    Update bit_operation_practice_3

--- 09.Bit.Operation/bit_operation_practice_3.c
+++ 09.Bit.Operation/bit_operation_practice_3.c
@@ -7,17 +7,10 @@ void div_num(unsigned int num, int *first, int *second, int *third){
 	*third = (num << 20) >> 20;
 }
 
-int check_nth_bit(unsigned int num, int n){
-	return (num >> (n-1)) & 1;
-}
-int invert_nth_bit(unsigned int num, int n){
-	return check_nth_bit(num, n) ^ 1;
-	// (~num >> (n-1)) & 1;
-}
 int main(void){
 	unsigned int num = 0x29387200;
 	int first, second, third;
 	div_num(num, &first, &second, &third);
-	printf("%d %d %d", first, second, third);
+	printf("%d %d %d\n", first, second, third);
 	return 0;
 }
