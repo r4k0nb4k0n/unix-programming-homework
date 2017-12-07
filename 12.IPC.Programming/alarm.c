9deb0f7dc32400786921e48742abd3b1f0843a6f
Choe Hyeong Jin <chj6097@gmail.com>
2017-12-07 12:26:05 +0900

    Comment alarm.c

--- 12.IPC.Programming/alarm.c
+++ 12.IPC.Programming/alarm.c
@@ -11,8 +11,9 @@ int signo;
 main()
 {
    signal(SIGALRM, alm);
-   alarm(10);
-   printf("process pause\n");
+   alarm(10); // kernel이 칼같이 알람시그널 보내준다.
+   printf("process pause\n"); // 누가 먼저 실행되느냐에 따라 영원히 멈출수도 있다.
+   // race condition
    pause();
    printf("process wakeup");
 }
