aa077639f403c689cfad0ce44706e77a93f9d9ef
Choe Hyeong Jin <chj6097@gmail.com>
2017-12-20 22:38:34 +0900

    Add src/prog

--- 10.Process.Management/vfork/vfork.c
+++ 10.Process.Management/vfork/vfork.c
@@ -1,3 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <stdio.h>
+
+static int glob = 6;
+
+int main(void)
+{
+	int var;
+	pid_t pid;
+
+	var = 88;
+	printf("before fork\n");
+
+	if( (pid = vfork()) < 0 )
+		printf("fork error\n");
+	else if( pid == 0) {
+		glob++;
+		var++;
+		_exit(0);
+	}
+	printf("pid = %d, glob = %d, var = %d\n", getpid(), glob, var);
+	exit(0);
+}
