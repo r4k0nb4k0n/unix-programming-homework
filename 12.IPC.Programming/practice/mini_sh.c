a8bdc1958a54b6b3f82669c8045fc4d9c9076099
Choe Hyeong Jin <chj6097@gmail.com>
2017-12-26 18:46:27 +0900

    Comment src

--- 12.IPC.Programming/practice/mini_sh.c
+++ 12.IPC.Programming/practice/mini_sh.c
@@ -37,9 +37,9 @@ int get_token(char **outptr)
 	switch (*ptr++) {
 		case '\0' : type = EOL; break;
 		case '&': type = AMPERSAND; break;
-		case '<': type = RDIN; break;
-		case '>': type = RDOUT; break;
-		case '|': type = PIPE; break;
+		case '<': type = RDIN;  break; /* 리다이렉션 토큰 추가 */
+		case '>': type = RDOUT; break; /* 리다이렉션 토큰 추가 */
+		case '|': type = PIPE;  break; /*     파이프 토큰 추가 */
 		default : type = ARG;
 			while ((*ptr != ' ') && (*ptr != '&') &&
 				(*ptr != '\t') && (*ptr != '\0'))
@@ -79,9 +79,9 @@ int parse_and_execute(char *input)
 	int	 quit = FALSE;
 	int	 narg = 0;
 	int	 finished = FALSE;
-  int  pid[2], p[2];
-  int  fd;
-  int  status;
+	int  pid[2], p[2];
+	int  fd;
+	int  status;
 
 	ptr = input;
 	tok = tokens;
@@ -122,22 +122,23 @@ int parse_and_execute(char *input)
   			if (type == EOL)
   				finished = TRUE;
   			break; 
-  		case RDIN:
-        type = get_token(&arg[++narg]); 
-        pid[0] = fork();
-        if(pid[0] < 0){ /* Handling fork() error */
+  		case RDIN: /* 리다이렉션 인 */
+        type = get_token(&arg[++narg]); /* 2번째 인자 받아오기 */
+        pid[0] = fork(); /* 자식 프로세스 생성 */
+        if(pid[0] < 0) { /* Handling fork() error */
           perror("minish: fork error");
           exit(-1);
         }
         else if(pid[0] == 0) { /* child */
-          fd = open(arg[2], O_RDONLY);
+          fd = open(arg[2], O_RDONLY); 
+		  /* 2번째 인자로 받은 파일 읽기 전용으로 열기 */
           if(fd < 0) {
             perror("minish: open error");
             exit(-1);
           }
-          dup2(fd, STDIN_FILENO);
+          dup2(fd, STDIN_FILENO); /* 자식 프로세스의 표준입력을 arg[2]로 설정 */
           close(fd);
-          execlp(arg[0], arg[0], (char*)0);
+          execlp(arg[0], arg[0], (char*)0); /* 자식 프로세스로 arg[0] 실행 */
           perror("minish: command not found"); exit(-1);
         }
         else if(pid[0] > 0) { /* Parent: Shell */
@@ -145,69 +146,64 @@ int parse_and_execute(char *input)
             if(errno != EINTR) return -1;
         }
         return quit=FALSE;
-  		case RDOUT:
-        type = get_token(&arg[++narg]); 
-        pid[0] = fork();
+  		case RDOUT: /* 리다이렉션 아웃 */
+        type = get_token(&arg[++narg]); /* 2번째 인자 받아오기 */
+        pid[0] = fork(); /* 자식 프로세스 생성 */
         if(pid[0] < 0){ /* Handling fork() error */
           perror("minish: fork error");
           exit(-1);
         }
         else if(pid[0] == 0) { /* child */
           fd = open(arg[2], O_RDWR | O_CREAT | S_IROTH, 0644);
+		  /* 2번째 인자로 받은 파일을 열기(없으면 생성, 있으면 덮어쓰기) */
           if(fd < 0) {
             perror("minish: open error");
             exit(-1);
           }
-          dup2(fd, STDOUT_FILENO);
+          dup2(fd, STDOUT_FILENO); /* 자식 프로세스의 표준출력을 arg[2]로 설정 */
           close(fd);
-          execlp(arg[0], arg[0], (char*)0);
+          execlp(arg[0], arg[0], (char*)0); /* 자식 프로세스로 arg[0] 실행 */
           perror("minish: command not found"); exit(-1);
         }
         else if(pid[0] > 0) { /* Parent: Shell */
           while(waitpid(pid[0], NULL, 0) < 0)
             if(errno != EINTR) return -1;
         }
-  		  return quit=FALSE;
-      case PIPE:
-        type = get_token(&arg[++narg]);
+  		return quit=FALSE;
+      case PIPE: /* 파이프 */
+        type = get_token(&arg[++narg]); /* 2번째 인자 받아오기 */
         pipe(p);
-        pid[0] = fork();
+
+        pid[0] = fork(); /* 1번째 자식 프로세스 생성 */
         if(pid[0] == 0) {
-          dup2(p[1], STDOUT_FILENO);
+          dup2(p[1], STDOUT_FILENO); /* 1번째 자식 프로세스의 표준출력을 파이프의 출력으로 설정 */
           close(p[0]); close(p[1]);
-          execlp(arg[0], arg[0], (char*)0);
+          execlp(arg[0], arg[0], (char*)0); /* 1번째 자식 프로세스로 arg[0] 실행 */
           perror("minish: first command not found"); exit(-1);
         }
-        /*else if(pid[0] > 0){
-          while(waitpid(pid[0], NULL, 0) < 0)
-            if(errno != EINTR) return -1;
-        }*/
         
-        pid[1] = fork();
+        pid[1] = fork(); /* 2번째 자식 프로세스 생성 */
         if(pid[1] == 0) {
-          dup2(p[0], STDIN_FILENO);
+          dup2(p[0], STDIN_FILENO); /* 2번째 자식 프로세스의 표준입력을 파이프의 입력으로 설정 */
           close(p[0]); close(p[1]);
-          execlp(arg[2], arg[2], (char*)0);
+          execlp(arg[2], arg[2], (char*)0); /* 2번째 자식 프로세스로 arg[2] 실행 */
           perror("minish: second command not found"); exit(-1);
         }
-        /*else if(pid[1] > 0){ 
-          while(waitpid(pid[1], NULL, 0) < 0)
-            if(errno != EINTR) return -1;
-        }*/
-        close(p[0]); close(p[1]);
+        
+		close(p[0]); close(p[1]);
         while(waitpid(pid[0], NULL, 0) < 0)
           if(errno != EINTR) return -1;
         while(waitpid(pid[1], NULL, 0) < 0)
           if(errno != EINTR) return -1;
         return quit=FALSE;
-    }
-  }
+		}
+	}
 	return quit;
 }
 
 int main()
 {
-  char *arg[1024];
+	char *arg[1024];
 	int	 quit;
 
 	printf("msh # ");
